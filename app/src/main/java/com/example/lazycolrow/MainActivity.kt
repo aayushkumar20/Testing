19d78cdd9d443d9e746448445595c1e135693a97
aayushkumar20 <37871733+aayushkumar20@users.noreply.github.com>
2023-05-25 18:42:34 +0530

    Hellomm

---
+++ app/src/main/java/com/example/lazycolrow/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.lazycolrow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lazycolrow.ui.theme.LazyColRowTheme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColRowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SimpleListView()
+                }
+            }
+        }
+    }
+}
+
+data class FruitModel(val name:String, val image : Int)
+
+private val fruitsList = mutableListOf<FruitModel>()
+
+@Composable
+fun SimpleListView() {
+    fruitsList.add(FruitModel("Most Secure OS", android.R.drawable.arrow_down_float))
+    fruitsList.add(FruitModel("Apple iOS and Mac", R.drawable.apple))
+    fruitsList.add(FruitModel("Linux OS", R.drawable.linux))
+
+    LazyColumn() {
+        items(fruitsList) {
+                model ->
+            ListRow(model = model)
+        }
+    }
+}
+
+@Composable
+fun ListRow(model: FruitModel) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .background(Color.LightGray)
+    ) {
+        Image(
+            painter = painterResource(id = model.image),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .padding(5.dp)
+        )
+        Text(
+            text = model.name,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.White
+        )
+    }
+}
+
+
